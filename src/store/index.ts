a2fea82120b5efd710e06f83363852d7453d8575
Egidio Caprino <egidio.caprino@gmail.com>
2020-11-07 20:02:00 +0100

    feat: persist user session

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,14 @@
 import { createStore } from 'vuex';
+import VuexPersistence from 'vuex-persist';
 import { isProduction } from '@/utils/environment';
 import notificationModule from './notificationModule';
 import userModule from './userModule';
 import shopModule from './shopModule';
+import { RootState } from './types';
+
+const vuexLocal = new VuexPersistence<RootState>({
+  storage: window.localStorage,
+});
 
 export default createStore({
   modules: {
@@ -10,5 +16,6 @@ export default createStore({
     user: userModule,
     shop: shopModule,
   },
+  plugins: [vuexLocal.plugin],
   strict: !isProduction(),
 });
